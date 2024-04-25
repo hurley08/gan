e1fbc02bde00361c9dc6f004ee0d20e1cd34f498
hurley08 <intriqet@gmail.com>
2024-04-25 15:25:28 -0700

    init

--- src/gan/scratch.py
+++ src/gan/scratch.py
@@ -1,4 +1,4 @@
-#/scratch.py
+# /scratch.py
 
 import torch
 from torch import nn
@@ -10,11 +10,8 @@ torch.manual_seed(111)
 
 
 train_data_length = 1024
-train_data  = torch.zeros((train_data_lenth, 2))
+train_data = torch.zeros((train_data_lenth, 2))
 train_data[:, 0] = 2 * math.pi * torch.rand(train_data_length)
 train_data[:, 1] = torch.sin(train_data[:, 0])
 train_labels = torch.zeros(train_data_length)
-train_set = [
-    (train_data[i], train_labels[i]) for i in range(train_data_length)
-]
-
+train_set = [(train_data[i], train_labels[i]) for i in range(train_data_length)]

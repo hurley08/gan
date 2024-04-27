eb52e8d948a23883d742d7cbc54b45a567e982c0
hurley08 <intriqet@gmail.com>
2024-04-26 19:00:07 -0700

    made minor changes to launcher

--- src/gan/launcher.py
+++ src/gan/launcher.py
@@ -6,17 +6,19 @@ from torch import nn
 import math
 import matplotlib.pyplot as plt
 
-torch.manual_seed(111)
-
-
-train_data_length = 1024
-train_data = torch.zeros((train_data_length, 2))
-train_data[:, 0] = 2 * math.pi * torch.rand(train_data_length)
-train_data[:, 1] = torch.sin(train_data[:, 0])
-train_labels = torch.zeros(train_data_length)
-train_set = [(train_data[i], train_labels[i]) for i in range(train_data_length)]
 
 
 class Launcher():
-	def __init__():
-		print('Launchar has been instantiated)')
+	def __init__(self, seed=111, d_length=1024, dimensions=2):
+		torch.manual_seed(seed)
+		self.train_data_length = d_length
+		self.train_data =  torch.zeros((d_length, 2))
+		self.train_data[:, 0] = 2 * math.pi * torch.rand(d_length)
+		self.train_data[:, 1] = torch.sin(self.train_data[:, 0])
+		self.train_labels = torch.zeros(d_length)
+		self.train_set = [(self.train_data[i], self.train_labels[i]) for i in range(d_length)]
+
+		print('Launchar has been instantiated')
+
+
+l = Launcher()

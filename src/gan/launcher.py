cceffa4260e9811fc1548adbdeb54d7c006113a9
hurley08 <intriqet@gmail.com>
2024-04-26 18:37:37 -0700

    reorg and testing import combinations

---
+++ src/gan/launcher.py
@@ -0,0 +1,22 @@
+# /scratch.py
+
+import torch
+from torch import nn
+
+import math
+import matplotlib.pyplot as plt
+
+torch.manual_seed(111)
+
+
+train_data_length = 1024
+train_data = torch.zeros((train_data_length, 2))
+train_data[:, 0] = 2 * math.pi * torch.rand(train_data_length)
+train_data[:, 1] = torch.sin(train_data[:, 0])
+train_labels = torch.zeros(train_data_length)
+train_set = [(train_data[i], train_labels[i]) for i in range(train_data_length)]
+
+
+class Launcher():
+	def __init__():
+		print('Launchar has been instantiated)')

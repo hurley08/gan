47b23d951fd3c81f0e1822dd4bf5620b64cbaae9
hurley08 <intriqet@gmail.com>
2024-04-26 18:50:45 -0700

    discriminator

---
+++ src/gan/discriminator.py
@@ -0,0 +1,30 @@
+#/discriminator.py
+
+import torch
+from torch import nn
+
+import math
+import matplotlib.pyplot as plt
+
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(2,256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(128,64),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(64, 1),
+            nn.Sigmoid(),
+        )
+    def forward(self, x):
+        output = self.model(x)
+        return output
+

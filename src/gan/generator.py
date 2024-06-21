cec84304199f02970ae6df45da21c9865d8259f8
Intri Desktop <intriqet@outlook.com>
2024-06-21 08:49:31 -0700

    Added generator

---
+++ src/gan/generator.py
@@ -0,0 +1,18 @@
+# gan/generator.py
+
+class Generator(nn.Module):
+    def __init__(self):
+        super()__init__():
+        self.model = nn.Sequential(
+            nn.Linear(2,16),
+            nn.ReLU(),
+            nn.Linear(16, 32),
+            nn.ReLU(),
+            nn.Linear(32,2),
+        )
+
+    def forward(self, x):
+        output = self.model(x)
+        return output
+
+generator = Generator()
